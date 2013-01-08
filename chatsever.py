2fbd43c24b3fdcdf7e5f36072f0c66a636e07558
Brandon Thomas Suit <brandon.suit@gmail.com>
2013-01-08 15:45:56 -0500

    Basic Server Sent Events chat gateway

---
+++ chatsever.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+"""
+A Server-Sent Events Chat App.
+ISIS (c) Brand Thomas <bt@brand.io>
+"""
+
+import os
+import time
+import datetime
+
+import flask
+import redis
+
+from flask import Flask, request, session, g, app, \
+	redirect, url_for, abort, render_template, flash, \
+	Response
+
+from jinja2 import evalcontextfilter, contextfilter
+from jinja2 import environmentfilter
+
+"""
+CONFIGURATION
+"""
+
+DEBUG = True
+SECRET_KEY = 'not a secret'
+
+"""
+GLOBALS
+"""
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+rds = redis.Redis()
+
+"""
+REQUEST PREPROCESSORS
+"""
+
+@app.before_request
+def before_request():
+	pass
+
+@app.teardown_request
+def teardown_request(exception):
+	pass
+
+@app.teardown_request
+def shutdown_session(exception=None):
+	pass
+
+"""
+BROWSER GATEWAY
+"""
+
+@app.errorhandler(404)
+def not_found(e):
+	return render_template('404.html'), 404
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if 'user' not in session:
+		return redirect('/login')
+
+	return redirect('/chat')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		session['user'] = request.form['user']
+		return redirect('/chat')
+
+	return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+	del session['user']
+	return redirect('/')
+
+@app.route('/chat')
+def chat():
+	if 'user' not in session:
+		return redirect('/')
+
+	return render_template('chat.html',
+			user=session['user'])
+
+"""
+AJAX GATEWAY
+"""
+
+@app.route('/send', methods=['POST'])
+def send():
+	user = session['user']
+	msg = request.form['message']
+
+	# TODO: No. 
+	now = datetime.datetime.now().replace(microsecond=0).time()
+
+	rds.publish('chatroom', '[%s] %s: %s' % (now.isoformat(), user, msg))
+
+	return ''
+
+
+@app.route('/stream')
+def stream():
+
+	def event_stream():
+		pubsub = rds.pubsub()
+		pubsub.subscribe('chatroom')
+		for msg in pubsub.listen():
+			return 'data: %s\n\n' % msg['data']
+
+	return Response(event_stream(),
+			mimetype='text/event-stream')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',
+			port=5550,
+			debug=True,
+			threaded=True)
+
---
+++ static/chat.js
@@ -0,0 +1,24 @@
+console.log('chat.js installed');
+
+var main = function() {
+	var source = new EventSource('/stream');
+
+	source.onmessage = function(ev) {
+		$('#chat').append('<div>' + ev.data + '</div>');
+	}
+
+	$('form').submit(function(ev) {
+		console.log('form submit');
+
+		var msg = $('#input').val();
+
+		$.post('/send', {message: msg});
+		$('#input').val('');
+
+		ev.preventDefault();
+		return false;
+	});
+}
+
+// Go, go, go!
+$(main());
